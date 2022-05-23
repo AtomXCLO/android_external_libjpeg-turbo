mirrorstogooglesource/libjpeg_turbo

Update libjpeg-turbo to upstream stable release 2.1.3
This release consists mostly of bug fixes and changes to make the
codebase comply with the various C standards.

There is also a very modest improvement to AArch64 Neon Huffman
encoding performance.

Change-Id: I21aa2dcb0f80a8095b77eb07b34e8ee02f9fb55d

jconfigint.h
@@ -26,9 +26,10 @@
 #define PACKAGE_NAME  "libjpeg-turbo"
 
 /* Version number of package */
-#define VERSION  "2.1.2"
+#define VERSION  "2.1.3"
 
 /* The size of `size_t', as computed by sizeof. */
+#include <limits.h>
 #if __WORDSIZE==64 || defined(_WIN64)
 #define SIZEOF_SIZE_T  8
 #else

AtomXCLO/android_external_libjpeg-turbo

jconfigint.h
@@ -26,9 +26,10 @@
 #define PACKAGE_NAME  "libjpeg-turbo"
 
 /* Version number of package */
-#define VERSION  "2.1.2"
+#define VERSION  "2.1.3"
 
 /* The size of `size_t', as computed by sizeof. */
+#include <stdint.h>
 #if __WORDSIZE==64 || defined(_WIN64)
 #define SIZEOF_SIZE_T  8
 #else